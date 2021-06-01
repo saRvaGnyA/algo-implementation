ef7012a4a11b8ae7910fb792ac9e79e481ab31dd
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2021-06-01 21:25:42 +0530

    Adding bubble sort algorithm in this commit

---
+++ sorting/bubble sort/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void bubble_sort(int[], int);
+
+int main() {
+    int size{};
+    cout << "Enter size of the array: ";
+    cin >> size;
+    int *a = new int[size];
+    cout << "Enter elements of the array: " << endl;
+    for(int count{}; count < size; ++count)
+        cin >> a[count];
+    cout << "The elements of the array are: " << endl;
+    for(int count{}; count < size; ++count)
+        cout << a[count] << "\t";
+    bubble_sort(a, size);
+    cout << "The elements of the sorted array are: " << endl;
+    for(int count{}; count < size; ++count)
+        cout << a[count] << "\t";
+    delete [] a;
+    return 0;
+}
+
+void bubble_sort(int arr[], int n) {
+    for(int i{}; i<n; ++i) {
+        for(int j{}; j<(n-i-1); ++j) {
+            if(arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
